529d8fa2bd7049179d996e212aacf567bc450668
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-08-24 20:11:43 -0500

    docs

--- a_plus.py
+++ a_plus.py
@@ -6,34 +6,50 @@ def h(nodo, meta):
     # Ademas representa adecuadamente el numero de movimientos 'ideales' para llegar a la meta
     return abs(nodo[0] - meta[0]) + abs(nodo[1] - meta[1])
 
-def f(g, nodo, lista_h):
-    return g[nodo] + lista_h[nodo]
-
 def calcular_heuristica(nodos, meta):
+    # Funcion de conveniencia para inicializar una lista con la funcion h
     lista_h = {}
     for n in nodos:
         lista_h[n] = h(n, meta)
     return lista_h
 
+def f(g, nodo, lista_h):
+    # Funcion de evaluacion f(n) = g(n) + h(n)
+    return g[nodo] + lista_h[nodo]
+
+
 
 def a_plus(lista_adjacencia, inicio, meta):
+
+    # Lista de nodos y calculo de heuristica
     nodos = [n for n, _ in lista_adjacencia.items()]
     lista_h = calcular_heuristica(nodos, meta)
+
+    # Lista de padres para reconstruir el camino
     padres = {}
+
+    # Funcion g que evalua el costo del camino desde el nodo inicial hasta el nodo actual
     g = {inicio: 0}
 
     pq = []
 
     padres[inicio] = None
+
+    # Metodo de python para usar una cola de prioridad
     heapq.heappush(pq, (f(g, inicio, lista_h), inicio))
 
     while pq:
         _, u = heapq.heappop(pq)
+
+        # Camino encontrado
         if u == meta:
             break
-        
+
+        # Actualziacion de funcion g
         nuevo_g = g[u] + 1
         for v in lista_adjacencia[u]:
+
+            # Si el nodo no ha sido visitado, o si se llega a este nodo de manera mas eficiente
             if v not in g or nuevo_g < g[v]:
                 g[v] = nuevo_g
                 padres[v] = u
@@ -41,8 +57,8 @@ def a_plus(lista_adjacencia, inicio, meta):
 
     camino = []
 
+    # Reconstruccion del camino
     it = meta
-
     while it is not None:
         camino.append(it)
         it = padres.get(it)
